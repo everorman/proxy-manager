cecdc9f54e57b228bfb6f66d1ed2b00f98897ea7
Evert Ortiz <evert.ortiz.m@gmail.com>
2022-03-06 20:22:33 -0500

    Renombra carpeta de backend para iniciar con express

--- backend-lb/src/controllers/user.controller.ts
+++ backend-lb/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 // Copyright IBM Corp. 2020. All Rights Reserved.
 
-import { service } from "@loopback/core";
+import { inject, service } from "@loopback/core";
 import { repository } from "@loopback/repository";
 import { HttpErrors, post, requestBody } from "@loopback/rest";
 import { genSalt, hash } from "bcryptjs";
@@ -8,13 +8,21 @@ import { User } from "../models";
 import { AuthService } from "../services/auth.service";
 import { Credentials } from "../types";
 import _ from 'lodash';
-import { UserRepository } from "@loopback/authentication-jwt";
+import { MyUserService, TokenServiceBindings, UserRepository, UserServiceBindings } from "@loopback/authentication-jwt";
+import { TokenService } from "@loopback/authentication";
+import {SecurityBindings, UserProfile} from '@loopback/security';
 
 export class UserController {
   constructor(
-    @repository(UserRepository) public userRepository: UserRepository,
-    @service(AuthService) public authService: AuthService
-  ) { }
+    @inject(TokenServiceBindings.TOKEN_SERVICE)
+    public jwtService: TokenService,
+    @inject(UserServiceBindings.USER_SERVICE)
+    public userService: MyUserService,
+    @inject(SecurityBindings.USER, {optional: true})
+    public user: UserProfile,
+    @repository(UserRepository) protected userRepository: UserRepository,
+    @service(AuthService) protected authService: AuthService
+  ) {}
 
   @post('/login', {
     responses: {
@@ -39,6 +47,7 @@ export class UserController {
   async signUp(
     @requestBody() user: User
   ) {
+    
     const password = await hash(user.password, await genSalt());
     user.password = password;
     const savedUser = await this.userRepository.create(
