165a912ae92f62edaa7ae8114aef54253eb6748f
Evert Ortiz <evert.ortiz.m@gmail.com>
2022-03-24 21:57:51 -0500

    corrige authrouters

--- backend-orm/src/routes/auth.routes.ts
+++ backend-orm/src/routes/auth.routes.ts
@@ -3,6 +3,7 @@ import { AuthController } from "../controller/authController";
 
 const router = new Router();
 
-router.post("/login", AuthController.login());
+router.post("/login", AuthController.login);
+router.post("/register", AuthController.register);
 
 export default router;
