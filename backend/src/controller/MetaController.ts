485506ca88747f3936c82298268eb4340ee1ff0c
Evert Ortiz <evert.ortiz.m@gmail.com>
2023-02-21 10:08:15 -0500

    Receiving the proxi to reset in the request body

--- backend/src/controller/MetaController.ts
+++ backend/src/controller/MetaController.ts
@@ -9,7 +9,9 @@ export class MetaController {
   private metaRepository = getRepository(Meta);
 
   async rotate(request: Request, response: Response) {
-    const externalUrl = await this.metaRepository.findOne({key: 'rotate-ip'});
+    const externalUrl = await this.metaRepository.findOne({ key: 'rotate-ip' });
+    const { id } = request.body;
+    const url = externalUrl.value.replace('{id}', id)
     await axios.get(externalUrl.value);
     return {
       status: 200,
