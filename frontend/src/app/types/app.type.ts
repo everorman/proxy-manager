c9d7b173f367bfabc3d7422c3f23ae0196c996ea
Evert Ortiz <evert.ortiz@encora.com>
2023-03-29 11:17:20 -0500

    Adding app types

---
+++ frontend/src/app/types/app.type.ts
@@ -0,0 +1,46 @@
+export type PageType = {
+    description: string;
+    organization: string;
+    region: string;
+    ip: string;
+    score: number;
+    fraud_score?: number;
+    id? :string;
+  }
+  
+  export type CurrentIpType = {
+    ip: string;
+  }
+  
+  export type UserRegisterType = {
+    firsName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    passwordRepeat: string;
+  }
+  
+  export type AuthJwtType = {
+    accesToken: string;
+    expiresIn: string;
+  }
+  
+  export type StatusRequestType = {
+    code: -1|-2|1;
+    message: string;
+    data: any;
+  }
+  
+  export type PaginationRequestType<T> = {
+    currentPage: number;
+    totalPages: number;
+    totalItems?: number;
+    result: T[]
+  }
+  
+  export type AlertType = {
+    type: 'danger' | 'danger' | 'warning' | 'info' | 'success';
+    msg: string;
+    timeout: number;
+  }
+  
